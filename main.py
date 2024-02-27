91f46f9f47bb3c0e9c25e678130c20ecc14450dd
Danil_Krupin <133006108+Rokyl59@users.noreply.github.com>
2024-02-27 20:45:14 +0500

    Исправил баг при вводе ссылки с протоколом

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ from dotenv import load_dotenv
 import requests
 import os
 import argparse
+from urllib.parse import urlparse
 
 
 def is_bitlink(token, url):
+    parser_url = urlparse(url)
+    bitlink = f"{parser_url.netloc}{parser_url.path}"
     headers = {"Authorization": f"Bearer {token}"}
     response = requests.get(
         f"https://api-ssl.bitly.com/v4/bitlinks/{url}",
@@ -22,14 +25,16 @@ def shorten_link(token, long_url):
         headers=headers
     )
     response.raise_for_status()
-    return response.json().get("id")
+    return response.json().get("link")
 
 
 def count_clicks(token, bitlink):
+    parsed_bitlink = urlparse(bitlink)
+    clean_bitlink = f"{parsed_bitlink.netloc}{parsed_bitlink.path}"
     headers = {"Authorization": f"Bearer {token}"}
     params = {"unit": "day", "units": -1, "size": 1}
     response = requests.get(
-        f"https://api-ssl.bitly.com/v4/bitlinks/{bitlink}/clicks/summary",
+        f"https://api-ssl.bitly.com/v4/bitlinks/{clean_bitlink}/clicks/summary",
         headers=headers,
         params=params
     )
@@ -52,7 +57,9 @@ def main():
     }
 
     try:
-        is_link = is_bitlink(bitly_token, user_input_url)
+        parsed_url = urlparse(user_input_url)
+        formatted_url = f"{parsed_url.netloc}{parsed_url.path}"
+        is_link = is_bitlink(bitly_token, formatted_url)
         func, message = actions[is_link]
         result = func(bitly_token, user_input_url)
         print(message.format(
